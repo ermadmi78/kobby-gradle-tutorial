667a415e100515085475d5e751df70318a600667
dermakov <ermadmi78@gmail.com>
2025-01-27 14:32:58 +0300

    Kobby release 4.1.1

--- cinema-api/build.gradle.kts
+++ cinema-api/build.gradle.kts
@@ -2,7 +2,7 @@ description = "Cinema API"
 
 plugins {
     kotlin("jvm")
-    id("io.github.ermadmi78.kobby") version "4.0.3"
+    id("io.github.ermadmi78.kobby") version "4.1.1"
 }
 
 kotlin {
@@ -14,5 +14,5 @@ dependencies {
     compileOnly("com.fasterxml.jackson.core:jackson-annotations:2.15.4")
 
     // Add this dependency to enable default Ktor adapters generation
-    compileOnly("io.ktor:ktor-client-cio:2.3.9")
+    compileOnly("io.ktor:ktor-client-cio:2.3.13")
 }
--- cinema-client/build.gradle.kts
+++ cinema-client/build.gradle.kts
@@ -19,10 +19,10 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.4")
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names:2.15.4")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
-    implementation("io.ktor:ktor-client-cio:2.3.9")
-    implementation("io.ktor:ktor-client-websockets:2.3.9")
+    implementation("io.ktor:ktor-client-cio:2.3.13")
+    implementation("io.ktor:ktor-client-websockets:2.3.13")
 
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
