1813dbcdb753bd7ca7c2145a8f7157bba1696609
dermakov <ermadmi78@gmail.com>
2025-06-01 16:09:49 +0300

    Kobby release 5.2.0

--- cinema-api/build.gradle.kts
+++ cinema-api/build.gradle.kts
@@ -2,18 +2,10 @@ import io.github.ermadmi78.kobby.kobby
 
 description = "Cinema API"
 
-buildscript {
-    dependencies {
-        classpath("io.github.ermadmi78:kobby-gradle-plugin:5.1.1-SNAPSHOT")
-    }
-}
-
 plugins {
     kotlin("jvm")
-    //id("io.github.ermadmi78.kobby") version "5.0.0"
+    id("io.github.ermadmi78.kobby") version "5.2.0"
 }
-
-apply(plugin = "io.github.ermadmi78.kobby")
 
 kotlin {
     jvmToolchain(17)
